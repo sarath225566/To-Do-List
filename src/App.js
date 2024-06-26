ca1932ffeff2cb40119e6431e05b66372b70fc50
sarath <sarath2255369@gmail.com>
2024-06-26 22:58:39 +0530

    style and update list show below

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
 
   const [toDos,setTodos]=useState([])
   const [toDo,setTodo]=useState('')
+
+  const addItem = () => {
+    setTodos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+    setTodo(''); // Clear the input field
+  };
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -16,16 +22,24 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setTodos([...toDos,toDo])} className="fas fa-plus"></i>
+        <i onClick={addItem} className="fas fa-plus"></i>
       </div>
       <div className="todos">
         {
-          toDos.map((value)=>{
+          toDos.map((obj)=>{
             return(
               <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>{value}</p>
+            <input value={obj.status}
+             onChange={(e)=>{
+              setTodos(toDos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                    obj2.status=e.target.checked;
+                }
+                return obj2;
+              }))
+            }} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
@@ -34,6 +48,15 @@ function App() {
             )
           })
         }
+
+        {toDos.map(obj=>{
+          if(obj.status){
+            return (
+              <h1 style={{color:'red'}}>{obj.text}</h1>
+            )
+          }
+          return null
+        })}
         
       </div>
     </div>
